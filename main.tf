ac786d1c4f5cb5407378c85c6f111832d1c94bf4
unknown <jchuks69@gmail.com>
2023-08-22 13:12:15 +0100

    bug fix

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = jenkins
+  key_name      = "jenkins"
   tags = {
     Name = "terraform-cicd"
   }
